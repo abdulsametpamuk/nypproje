cc8e2f4b1edb428b4281ca53bf9bc677d88de10d
abdulsametpamuk <21181616015@gazi.edu.tr>
2025-11-28 12:16:46 +0300

    sametdeneme4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -15,5 +17,6 @@ public class Main {
 
         System.out.println("main");
         System.out.println("Samet deneme 1");
+        System.out.println("Samet deneme 2");
     }
 }
